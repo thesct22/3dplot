2731ac0772e0b5e4e203114cd7dbb08b70d8a887
thesct22 <sharathct22@gmail.com>
2020-02-11 14:07:05 +0530

    blah

--- sample.py
+++ sample.py
@@ -164,33 +164,13 @@ export class Surface3d extends LayoutDOM {
 }
 """
 
-# This custom extension model will have a DOM view that should layout-able in
-# Bokeh layouts, so use ``LayoutDOM`` as the base class. If you wanted to create
-# a custom tool, you could inherit from ``Tool``, or from ``Glyph`` if you
-# wanted to create a custom glyph, etc.
+
 class Surface3d(LayoutDOM):
 
-    # The special class attribute ``__implementation__`` should contain a string
-    # of JavaScript (or CoffeeScript) code that implements the JavaScript side
-    # of the custom extension model.
     __implementation__ = TypeScript(TS_CODE)
 
-    # Below are all the "properties" for this model. Bokeh properties are
-    # class attributes that define the fields (and their types) that can be
-    # communicated automatically between Python and the browser. Properties
-    # also support type validation. More information about properties in
-    # can be found here:
-    #
-    #    https://docs.bokeh.org/en/latest/docs/reference/core/properties.html#bokeh-core-properties
-
-    # This is a Bokeh ColumnDataSource that can be updated in the Bokeh
-    # server by Python code
     data_source = Instance(ColumnDataSource)
 
-    # The vis.js library that we are wrapping expects data for x, y, and z.
-    # The data will actually be stored in the ColumnDataSource, but these
-    # properties let us specify the *name* of the column that should be
-    # used for each field.
     x = String
 
     y = String
