b8dc96d369b827c56047c280cac1f6e26a0dd327
Sharath Cherian Thomas <sharathct22@gmail.com>
2020-03-30 11:22:43 +0530

    Update runthis.py

--- runthis.py
+++ runthis.py
@@ -1,10 +1,5 @@
 from firebase import firebase
 from requests import get
-<<<<<<< HEAD
 firebase= firebase.FirebaseApplication('https://air-conditioning-12b11.firebaseio.com/')
 ip = get('https://api.ipify.org').text
 result1 = firebase.put('https://air-conditioning-app.firebaseio.com/','ip',ip)
-=======
-ip = get('https://api.ipify.org').text
-result1 = firebase.put('https://air-conditioning-app.firebaseio.com/','ip',ip)
->>>>>>> 022a22a43368e6d5e0e3255a66704c0a9649f6c0
